a3db63e99107f5a01673184e2672596333a4d9cf
jonny <jonny@pinae.net>
2014-04-28 12:13:26 +0900

    Alittle bit of styling

--- js/cpuCtrl.js
+++ js/cpuCtrl.js
@@ -3,9 +3,8 @@
 bonsaiApp.controller('bonsaiCpuCtrl',
     function ($scope, $routeParams) {
         $scope.dataBusHandler = {};
+        $scope.adressBusHandler = {};
 
         $scope.text = "Hallo Welt!";
-        $scope.value1 = 345678;
-        $scope.value2 = 123456;
     }
 );
