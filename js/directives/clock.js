dabc7420d6134212bb1f3687c7f6b33751edb0e6
jonny <jonny@pinae.net>
2014-08-02 19:45:37 +0900

    Adjusted Clock design to work in FF.

--- js/directives/clock.js
+++ js/directives/clock.js
@@ -48,7 +48,7 @@ bonsaiApp.directive('clock', function ($interval) {
             });
 
             $scope.$watch('top', function () {
-                $scope.topCSS = ($scope.top - 9) + 'px';
+                $scope.topCSS = ($scope.top - 10) + 'px';
             });
             $scope.$watch('left', function () {
                 $scope.leftCSS = ($scope.left + 3) + 'px';
