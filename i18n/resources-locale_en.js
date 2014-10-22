3a27661d8a136fc2c61dcddb10e575d48d676185
jonny <jonny@pinae.net>
2014-10-22 15:36:56 +0900

    Default language as a symlink

---
+++ i18n/resources-locale_en.js
@@ -0,0 +1,392 @@
+﻿[
+    {
+        "key"        : "_bonsai_",
+        "value"      : "Bonsai",
+        "description": ""
+    },
+    {
+        "key"        : "_CpuView_",
+        "value"      : "Circuit",
+        "description": ""
+    },
+    {
+        "key"        : "_errors_",
+        "value"      : "Errors:",
+        "description": ""
+    },
+    {
+        "key"        : "_clearErrors_",
+        "value"      : "clear",
+        "description": ""
+    },
+    {
+        "key"        : "_saveCpuButton_",
+        "value"      : "save CPU to file",
+        "description": ""
+    },
+    {
+        "key"        : "_clearCpuButton_",
+        "value"      : "clear CPU",
+        "description": ""
+    },
+    {
+        "key"        : "_BusOccupiedException_beginning",
+        "value"      : "This bus (",
+        "description": ""
+    },
+    {
+        "key"        : "_BusOccupiedException_middle",
+        "value"      : ") is already occupied by ",
+        "description": ""
+    },
+    {
+        "key"        : "_BusOccupiedException_end",
+        "value"      : ".",
+        "description": ""
+    },
+    {
+        "key"        : "_NotEnrolledReadException_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_NotEnrolledReadException_middle",
+        "value"      : " is not enrolled to the bus (",
+        "description": ""
+    },
+    {
+        "key"        : "_NotEnrolledReadException_end",
+        "value"      : ") and can not read.",
+        "description": ""
+    },
+    {
+        "key"        : "_NotEnrolledWriteException_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_NotEnrolledWriteException_middle",
+        "value"      : " is not enrolled to the bus (",
+        "description": ""
+    },
+    {
+        "key"        : "_NotEnrolledWriteException_end",
+        "value"      : ") and can not write.",
+        "description": ""
+    },
+    {
+        "key"        : "_RegisterIsAlreadyReadingException_beginning",
+        "value"      : "Register ",
+        "description": ""
+    },
+    {
+        "key"        : "_RegisterIsAlreadyReadingException_middle",
+        "value"      : " is already reading.",
+        "description": ""
+    },
+    {
+        "key"        : "_RegisterIsAlreadyReadingException_end",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_ControlWireNoWriterSpecifiedException_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_ControlWireNoWriterSpecifiedException_middle",
+        "value"      : " has no writer specified. This means the control wire is in an illegal state.",
+        "description": ""
+    },
+    {
+        "key"        : "_ControlWireNoWriterSpecifiedException_end",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_ControlWireWrongWriterException_beginning",
+        "value"      : "The writer you have given does not match the writer (",
+        "description": ""
+    },
+    {
+        "key"        : "_ControlWireWrongWriterException_middle",
+        "value"      : ") of this control wire (",
+        "description": ""
+    },
+    {
+        "key"        : "_ControlWireWrongWriterException_end",
+        "value"      : ").",
+        "description": ""
+    },
+    {
+        "key"        : "_AddressBusConnectionCanNotBeSetToWrite_beginning",
+        "value"      : "Memory ",
+        "description": ""
+    },
+    {
+        "key"        : "_AddressBusConnectionCanNotBeSetToWrite_middle",
+        "value"      : ": It does not make sense to write to an address bus (",
+        "description": ""
+    },
+    {
+        "key"        : "_AddressBusConnectionCanNotBeSetToWrite_end",
+        "value"      : ").",
+        "description": ""
+    },
+    {
+        "key"        : "_NoAdressBusConnected_beginning",
+        "value"      : "Memory ",
+        "description": ""
+    },
+    {
+        "key"        : "_NoAdressBusConnected_middle",
+        "value"      : ": No address bus connected. You have to specify an address which specifies where to write or read the data.",
+        "description": ""
+    },
+    {
+        "key"        : "_NoAdressBusConnected_end",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_InactiveAdressBusConnection_beginning",
+        "value"      : "Memory ",
+        "description": ""
+    },
+    {
+        "key"        : "_InactiveAdressBusConnection_middle",
+        "value"      : ": The address bus connection is not set to reading. This connection needs to be set to reading to get an address from the address bus which specifies where to write or read.",
+        "description": ""
+    },
+    {
+        "key"        : "_InactiveAdressBusConnection_end",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedMaxValueIsNotANumber_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedMaxValueIsNotANumber_middle",
+        "value"      : ": The value you supplied (",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedMaxValueIsNotANumber_end",
+        "value"      : ") is not a Number. As max values only numbers are allowed.",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedValueIsNotANumber_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedValueIsNotANumber_middle",
+        "value"      : ": The value you supplied (",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedMaxValueIsNotANumber_end",
+        "value"      : ") is not a Number. As value only numbers are allowed.",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedValueIsNegative_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedValueIsNegative_middle",
+        "value"      : ": The value you supplied (",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedValueIsNegative_end",
+        "value"      : ") is negative. For this value only positive numbers are allowed.",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedValueIsTooBig_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedValueIsTooBig_middle",
+        "value"      : ": The value you supplied is too big. The maximum value in the current configuration is ",
+        "description": ""
+    },
+    {
+        "key"        : "_SuppliedValueIsTooBig_end",
+        "value"      : ".",
+        "description": ""
+    },
+    {
+        "key"        : "_BusNotFound_beginning",
+        "value"      : "The bus with ID ",
+        "description": ""
+    },
+    {
+        "key"        : "_BusNotFound_middle",
+        "value"      : " was not found.",
+        "description": ""
+    },
+    {
+        "key"        : "_BusNotFound_end",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_WrongValueForSingleBit_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_WrongValueForSingleBit_middle",
+        "value"      : ": ",
+        "description": ""
+    },
+    {
+        "key"        : "_WrongValueForSingleBit_end",
+        "value"      : " is not a valid value for a single bit. Only 0 or 1 are allowed.",
+        "description": ""
+    },
+    {
+        "key"        : "_DelayValueNotAnInteger_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_DelayValueNotAnInteger_middle",
+        "value"      : " is not an integer. For a delay only a positive number of milliseconds is allowed.",
+        "description": ""
+    },
+    {
+        "key"        : "_DelayValueNotAnInteger_end",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_DelayAlreadyOccupied_beginning",
+        "value"      : "This delay (",
+        "description": ""
+    },
+    {
+        "key"        : "_DelayAlreadyOccupied_middle",
+        "value"      : ") is already occupied by ",
+        "description": ""
+    },
+    {
+        "key"        : "_DelayAlreadyOccupied_end",
+        "value"      : ".",
+        "description": ""
+    },
+    {
+        "key"        : "_GateIsAlreadyWriting_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_GateIsAlreadyWriting_middle",
+        "value"      : ": The gate to bus ",
+        "description": ""
+    },
+    {
+        "key"        : "_GateIsAlreadyWriting_end",
+        "value"      : " is already writing and therefore can not be set to read.",
+        "description": ""
+    },
+    {
+        "key"        : "_GateIsAlreadyReading_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_GateIsAlreadyReading_middle",
+        "value"      : ": The gate to bus ",
+        "description": ""
+    },
+    {
+        "key"        : "_GateIsAlreadyReading_end",
+        "value"      : " is already reading and therefore can not be set to write.",
+        "description": ""
+    },
+    {
+        "key"        : "_RegisterIsReadingAndCantAcceptValueChanges_beginning",
+        "value"      : "Register ",
+        "description": ""
+    },
+    {
+        "key"        : "_RegisterIsReadingAndCantAcceptValueChanges_middle",
+        "value"      : " is Reading from a Bus and can not accept value Changes.",
+        "description": ""
+    },
+    {
+        "key"        : "_RegisterIsReadingAndCantAcceptValueChanges_end",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_WriteToUndefinedAddress_beginning",
+        "value"      : "Memory ",
+        "description": ""
+    },
+    {
+        "key"        : "_WriteToUndefinedAddress_middle",
+        "value"      : ": The address which was read from the bus (",
+        "description": ""
+    },
+    {
+        "key"        : "_WriteToUndefinedAddress_end",
+        "value"      : ") is undefined. Nothing can be written to a undefined place.",
+        "description": ""
+    },
+    {
+        "key"        : "_ValueIsMaxNoInc_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_ValueIsMaxNoInc_middle",
+        "value"      : ": The value is ",
+        "description": ""
+    },
+    {
+        "key"        : "_ValueIsMaxNoInc_end",
+        "value"      : " which is already the maximum value. Therefore increasing the value is not allowed.",
+        "description": ""
+    },
+    {
+        "key"        : "_ZeroValueNoDec_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_ZeroValueNoDec_middle",
+        "value"      : ": The value is ",
+        "description": ""
+    },
+    {
+        "key"        : "_ZeroValueNoDec_end",
+        "value"      : " and must not be negative. So decreasing this value is not allowed.",
+        "description": ""
+    },
+    {
+        "key"        : "_ValueNotInRangeZeroToMax_beginning",
+        "value"      : "",
+        "description": ""
+    },
+    {
+        "key"        : "_ValueNotInRangeZeroToMax_middle",
+        "value"      : ": The value you try to set is not in the Range 0 to ",
+        "description": ""
+    },
+    {
+        "key"        : "_ValueNotInRangeZeroToMax_end",
+        "value"      : ".",
+        "description": ""
+    }
+]
