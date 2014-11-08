1dd5558fd5958a6ee67da0091e73f13a14b450ea
jonny <jonny@pinae.net>
2014-11-08 23:49:35 +0900

    Code completion on character input

--- js/controllers/assemblerCtrl.js
+++ js/controllers/assemblerCtrl.js
@@ -3,7 +3,7 @@
 bonsaiApp.controller('bonsaiAssemblerCtrl',
     function ($scope, $timeout) {
         $scope.splitLines = function (string) {
-            return string.replace(/\r\n|\n\r|\n|\r/g,"\n").split("\n")
+            return string.replace(/\r\n|\n\r|\n|\r/g, "\n").split("\n")
         };
 
         $scope.program = "inc 14\ndec 12";
@@ -12,7 +12,7 @@ bonsaiApp.controller('bonsaiAssemblerCtrl',
         $scope.$watch('program', function (newText, oldText) {
             var lines = $scope.splitLines(newText);
             $scope.formattedProgram = [];
-            angular.forEach(lines, function (line) {
+            angular.forEach(lines, function (line, index) {
                 var matches = line.match(/^(inc|dec|tst|jmp)[ \t](\d+)$/);
                 if (matches) { // complete and correct line
                     $scope.formattedProgram.push({
@@ -22,39 +22,98 @@ bonsaiApp.controller('bonsaiAssemblerCtrl',
                         correct: true
                     })
                 } else {
-                matches = line.match(/^(inc|dec|tst|jmp)[ \t]?$/);
-                if (matches) { // incomplete line without completion
-                    $scope.formattedProgram.push({
-                        command: matches[1],
-                        completion: '',
-                        address: ''
-                    })
-                } else {
-                matches = line.match(/^(in?|de?|ts?|jm?)$/);
-                if (matches) { // incomplete line without completion
-                    var completion = '';
-                    if (matches[1] == 'in') { completion = 'c' }
-                    if (matches[1] == 'i') { completion = 'nc' }
-                    if (matches[1] == 'de') { completion = 'c' }
-                    if (matches[1] == 'd') { completion = 'ec' }
-                    if (matches[1] == 'ts') { completion = 't' }
-                    if (matches[1] == 't') { completion = 'st' }
-                    if (matches[1] == 'jm') { completion = 'p' }
-                    if (matches[1] == 'j') { completion = 'mp' }
-                    $scope.formattedProgram.push({
-                        command: matches[1],
-                        completion: completion,
-                        address: ''
-                    })
-                } else {
-                    $scope.formattedProgram.push({
-                        command: '',
-                        completion: '',
-                        address: ''
-                    })
-                }}}
+                    matches = line.match(/^(inc|dec|tst|jmp)[ \t]?$/);
+                    if (matches) { // incomplete line without completion
+                        $scope.formattedProgram.push({
+                            command: matches[1],
+                            completion: '',
+                            address: ''
+                        })
+                    } else {
+                        matches = line.match(/^(in?|de?|ts?|jm?)$/);
+                        if (matches) { // incomplete line without completion
+                            var completion = '';
+                            if (matches[1] == 'in') {
+                                completion = 'c'
+                            }
+                            if (matches[1] == 'i') {
+                                completion = 'nc'
+                            }
+                            if (matches[1] == 'de') {
+                                completion = 'c'
+                            }
+                            if (matches[1] == 'd') {
+                                completion = 'ec'
+                            }
+                            if (matches[1] == 'ts') {
+                                completion = 't'
+                            }
+                            if (matches[1] == 't') {
+                                completion = 'st'
+                            }
+                            if (matches[1] == 'jm') {
+                                completion = 'p'
+                            }
+                            if (matches[1] == 'j') {
+                                completion = 'mp'
+                            }
+                            $scope.formattedProgram.push({
+                                command: matches[1],
+                                completion: completion,
+                                address: ''
+                            })
+                        } else {
+                            if ((oldText) && ($scope.splitLines(oldText)[index])) {
+                                matches = $scope.splitLines(oldText)[index].match(/^(in?|de?|ts?|jm?)$/);
+                                if (matches) {
+                                    var numberMatches = line.match(/^(in?|de?|ts?|jm?)([ \t]+)?(\d+)?$/);
+                                    if (numberMatches) {
+                                        var insertCommand = function (lineNumber, command, address) {
+                                            var countedLines = 0;
+                                            for (var i = 0; i < $scope.program.length; i++) {
+                                                if (countedLines == lineNumber) {
+                                                    var delIndex = $scope.program.indexOf("\n", i);
+                                                    if (delIndex < 0) {
+                                                        delIndex = $scope.program.length - 1;
+                                                    }
+                                                    $scope.program = $scope.program.slice(0, i) +
+                                                        command + ' ' + address +
+                                                        $scope.program.slice(delIndex + 1);
+                                                    return null;
+                                                }
+                                                if ($scope.program[i] == "\n") {
+                                                    countedLines++;
+                                                }
+                                            }
+                                        };
+                                        var address = '';
+                                        if (numberMatches[3]) {
+                                            address = numberMatches[3];
+                                        }
+                                        if (numberMatches[1][0] == 'i') {
+                                            insertCommand(index, 'inc', address);
+                                        }
+                                        if (numberMatches[1][0] == 'd') {
+                                            insertCommand(index, 'dec', address);
+                                        }
+                                        if (numberMatches[1][0] == 't') {
+                                            insertCommand(index, 'tst', address);
+                                        }
+                                        if (numberMatches[1][0] == 'j') {
+                                            insertCommand(index, 'jmp', address);
+                                        }
+                                    }
+                                }
+                                $scope.formattedProgram.push({
+                                    command: '',
+                                    completion: '',
+                                    address: ''
+                                })
+                            }
+                        }
+                    }
+                }
             });
-            console.log($scope.formattedProgram);
         });
 
         $scope.$watch('data', function (newText, oldText) {
