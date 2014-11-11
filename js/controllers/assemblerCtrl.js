3fd3aca5b2a67336adc66553a0c80086fcc43df1
jonny <jonny@pinae.net>
2014-11-11 22:15:47 +0900

    Syntax highlighting for hlt command

--- js/controllers/assemblerCtrl.js
+++ js/controllers/assemblerCtrl.js
@@ -36,99 +36,110 @@ bonsaiApp.controller('bonsaiAssemblerCtrl',
                         completion: '',
                         address: matches[2],
                         correct: true
-                    })
-                } else {
-                    matches = line.match(/^(inc|dec|tst|jmp)[ \t]?$/);
-                    if (matches) { // incomplete line without completion
-                        $scope.formattedProgram.push({
-                            command: matches[1],
-                            completion: '',
-                            address: ''
-                        })
-                    } else {
-                        matches = line.match(/^(in?|de?|ts?|jm?)$/);
-                        if (matches) { // incomplete line without completion
-                            var completion = '';
-                            if (matches[1] == 'in') {
-                                completion = 'c'
-                            }
-                            if (matches[1] == 'i') {
-                                completion = 'nc'
-                            }
-                            if (matches[1] == 'de') {
-                                completion = 'c'
-                            }
-                            if (matches[1] == 'd') {
-                                completion = 'ec'
-                            }
-                            if (matches[1] == 'ts') {
-                                completion = 't'
-                            }
-                            if (matches[1] == 't') {
-                                completion = 'st'
-                            }
-                            if (matches[1] == 'jm') {
-                                completion = 'p'
-                            }
-                            if (matches[1] == 'j') {
-                                completion = 'mp'
-                            }
-                            $scope.formattedProgram.push({
-                                command: matches[1],
-                                completion: completion,
-                                address: ''
-                            })
-                        } else {
-                            if ((oldText) && ($scope.splitLines(oldText)[index])) {
-                                matches = $scope.splitLines(oldText)[index].match(/^(in?|de?|ts?|jm?)$/);
-                                if (matches) {
-                                    var numberMatches = line.match(/^(in?|de?|ts?|jm?)([ \t]+)?(\d+)?$/);
-                                    if (numberMatches) {
-                                        var insertCommand = function (lineNumber, command, address) {
-                                            var countedLines = 0;
-                                            for (var i = 0; i < $scope.program.length; i++) {
-                                                if (countedLines == lineNumber) {
-                                                    var delIndex = $scope.program.indexOf("\n", i);
-                                                    if (delIndex < 0) {
-                                                        delIndex = $scope.program.length - 1;
-                                                    }
-                                                    $scope.program = $scope.program.slice(0, i) +
-                                                        command + ' ' + address +
-                                                        $scope.program.slice(delIndex + 1);
-                                                    return null;
-                                                }
-                                                if ($scope.program[i] == "\n") {
-                                                    countedLines++;
-                                                }
-                                            }
-                                        };
-                                        var address = '';
-                                        if (numberMatches[3]) {
-                                            address = numberMatches[3];
+                    });
+                    return null;
+                }
+                matches = line.match(/^(hlt)$/);
+                if (matches) {
+                    $scope.formattedProgram.push({
+                        command: matches[1],
+                        completion: '',
+                        address: '',
+                        correct: true
+                    });
+                    return null;
+                }
+                matches = line.match(/^(inc|dec|tst|jmp)[ \t]?$/);
+                if (matches) { // incomplete line without completion
+                    $scope.formattedProgram.push({
+                        command: matches[1],
+                        completion: '',
+                        address: ''
+                    });
+                    return null;
+                }
+                matches = line.match(/^(in?|de?|ts?|jm?)$/);
+                if (matches) { // incomplete line without completion
+                    var completion = '';
+                    if (matches[1] == 'in') {
+                        completion = 'c'
+                    }
+                    if (matches[1] == 'i') {
+                        completion = 'nc'
+                    }
+                    if (matches[1] == 'de') {
+                        completion = 'c'
+                    }
+                    if (matches[1] == 'd') {
+                        completion = 'ec'
+                    }
+                    if (matches[1] == 'ts') {
+                        completion = 't'
+                    }
+                    if (matches[1] == 't') {
+                        completion = 'st'
+                    }
+                    if (matches[1] == 'jm') {
+                        completion = 'p'
+                    }
+                    if (matches[1] == 'j') {
+                        completion = 'mp'
+                    }
+                    $scope.formattedProgram.push({
+                        command: matches[1],
+                        completion: completion,
+                        address: ''
+                    });
+                    return null;
+                }
+                if ((oldText) && ($scope.splitLines(oldText)[index])) {
+                    matches = $scope.splitLines(oldText)[index].match(/^(in?|de?|ts?|jm?)$/);
+                    if (matches) {
+                        var numberMatches = line.match(/^(in?|de?|ts?|jm?)([ \t]+)?(\d+)?$/);
+                        if (numberMatches) {
+                            var insertCommand = function (lineNumber, command, address) {
+                                var countedLines = 0;
+                                for (var i = 0; i < $scope.program.length; i++) {
+                                    if (countedLines == lineNumber) {
+                                        var delIndex = $scope.program.indexOf("\n", i);
+                                        if (delIndex < 0) {
+                                            delIndex = $scope.program.length - 1;
                                         }
-                                        if (numberMatches[1][0] == 'i') {
-                                            insertCommand(index, 'inc', address);
-                                        }
-                                        if (numberMatches[1][0] == 'd') {
-                                            insertCommand(index, 'dec', address);
-                                        }
-                                        if (numberMatches[1][0] == 't') {
-                                            insertCommand(index, 'tst', address);
-                                        }
-                                        if (numberMatches[1][0] == 'j') {
-                                            insertCommand(index, 'jmp', address);
-                                        }
+                                        $scope.program = $scope.program.slice(0, i) +
+                                            command + ' ' + address +
+                                            $scope.program.slice(delIndex + 1);
+                                        return null;
+                                    }
+                                    if ($scope.program[i] == "\n") {
+                                        countedLines++;
                                     }
                                 }
+                            };
+                            var address = '';
+                            if (numberMatches[3]) {
+                                address = numberMatches[3];
                             }
-                            $scope.formattedProgram.push({
-                                command: '',
-                                completion: '',
-                                address: ''
-                            });
+                            if (numberMatches[1][0] == 'i') {
+                                insertCommand(index, 'inc', address);
+                            }
+                            if (numberMatches[1][0] == 'd') {
+                                insertCommand(index, 'dec', address);
+                            }
+                            if (numberMatches[1][0] == 't') {
+                                insertCommand(index, 'tst', address);
+                            }
+                            if (numberMatches[1][0] == 'j') {
+                                insertCommand(index, 'jmp', address);
+                            }
                         }
                     }
                 }
+                $scope.formattedProgram.push({
+                    command: '',
+                    completion: '',
+                    address: ''
+                });
+                return null;
             });
         });
 
