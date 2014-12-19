709a73de8386d437206735bbdf72f4d2d347b9ad
jonny <jonny@pinae.net>
2014-12-19 18:42:51 +0900

    Reformatted app.js with smaller indents

--- js/app.js
+++ js/app.js
@@ -68,132 +68,135 @@ var menu = [
 ];
 
 var bonsaiApp = angular.module(
-        'Bonsai',
-        [
-            'ngAnimate',
-            'ngRoute',
-            'localization',
-            'ServerServices'
-        ]
-    )
-    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-        $routeProvider.when('/', {templateUrl: '/partials/introduction.html', controller: 'bonsaiPageCtrl'});
-        $routeProvider.when('/imprint/', {templateUrl: '/partials/imprint.html', controller: 'bonsaiPageCtrl'});
-        var registerMenuRoutes = function(menu) {
-            for (var i = 0; i < menu.length; i++) {
-                if (menu[i].link && menu[i].partial) {
-                    $routeProvider.
-                        when(menu[i].link, {templateUrl: '/partials/' + menu[i].partial, controller: 'bonsaiPageCtrl'});
-                }
-                if (menu[i].submenu) {
-                    registerMenuRoutes(menu[i].submenu);
-                }
+    'Bonsai',
+    [
+        'ngAnimate',
+        'ngRoute',
+        'localization',
+        'ServerServices'
+    ]
+);
+
+bonsaiApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {templateUrl: '/partials/introduction.html', controller: 'bonsaiPageCtrl'});
+    $routeProvider.when('/imprint/', {templateUrl: '/partials/imprint.html', controller: 'bonsaiPageCtrl'});
+    var registerMenuRoutes = function (menu) {
+        for (var i = 0; i < menu.length; i++) {
+            if (menu[i].link && menu[i].partial) {
+                $routeProvider.
+                    when(menu[i].link, {templateUrl: '/partials/' + menu[i].partial, controller: 'bonsaiPageCtrl'});
             }
-        };
-        registerMenuRoutes(menu);
-        $routeProvider.
-            when('/bonsai/', {templateUrl: '/partials/main.html', controller: 'bonsaiCpuCtrl'}).
-            when('/assembler/', {templateUrl: '/partials/assembler.html', controller: 'bonsaiAssemblerCtrl'}).
-            when('/bonsai/assembler/', {templateUrl: '/partials/assembler.html', controller: 'bonsaiAssemblerCtrl'});
-        $routeProvider.otherwise({templateUrl: '/partials/introduction.html', controller: 'bonsaiPageCtrl'});
-        // use the HTML5 History API
-		$locationProvider.html5Mode(true).hashPrefix('!');
-    }])
-    .factory('$exceptionHandler', function ($injector) {
-        return function (exception, cause) {
-            if (exception.localization) {
-                $injector.get('$rootScope').$broadcast('error', exception);
-            } else {
-                if (cause) {
-                    console.log("Cause: " + cause);
-                }
-                throw exception;
-            }
-        };
-    })
-    .run(function ($rootScope, $window, $document, $location, localize, PageCounter) {
-        // root scope functions
-        $rootScope.getLanguages = function () {
-            return ['en', 'de'];
-        };
-        $rootScope.setLanguage = function (lang) {
-            $rootScope.language = lang;
-        };
-        $rootScope.$watch('language', function (newLang) {
-            localize.setLanguage(newLang);
-            $rootScope.$broadcast('langChange', newLang);
-        });
-        var findPartial = function (path) {
-            if (path == '/') {
-                return 'introduction.html';
-            }
-            if (path == '/imprint/') {
-                return 'imprint.html';
-            }
-            var findPartialInMenu = function(path, menu) {
-                for (var i = 0; i < menu.length; i++) {
-                    if (menu[i].link == path) {
-                        return menu[i].partial
-                    }
-                    if (menu[i].submenu) {
-                        var found = findPartialInMenu(path, menu[i].submenu);
-                        if (found) {
-                            return found;
-                        }
-                    }
-                }
-                return false;
-            };
-            return findPartialInMenu(path, menu);
-        };
-        $rootScope.$watch(
-            function () {
-                return $location.path();
-            },
-            function (path) {
-                PageCounter.count(path, findPartial(path)).success(function (data) {
-                    if (data.count) {
-                        $rootScope.viewsCount = data.count;
-                    }
-                    if (data.last_change) {
-                        $rootScope.lastChange = new Date(data.last_change * 1000);
-                    }
-                });
-                var pathParts = path.split('/');
-                var currentMenu = [];
-                angular.forEach(menu, function(menuEntry) {
-                    if (menuEntry.name == pathParts[1]) {
-                        currentMenu = menu;
-                    }
-                });
-                if ((path == '/') || (path == '/imprint/')) {
-                    currentMenu = menu;
-                }
-                var navigationPath = [];
-                for (var i = 0; i < pathParts.length; i++) {
-                    if (pathParts[i]) {
-                        for (var j = 0; j < currentMenu.length; j++) {
-                            if ((currentMenu[j].name == pathParts[i]) && (currentMenu[j].submenu)) {
-                                navigationPath.push({'title': currentMenu[j].title, 'link': currentMenu[j].link});
-                                currentMenu = currentMenu[j].submenu;
-                            }
-                        }
-                    }
-                }
-                $rootScope.navigationPath = navigationPath;
-                $rootScope.currentPath = path;
-                $rootScope.menu = currentMenu;
-                $rootScope.author = undefined;
-            }
-        );
-        // initialization
-        if (!$rootScope.language) {
-            $rootScope.language = $window.navigator.userLanguage ||
-                $window.navigator.language ||
-                $document.getElementsByTagName('html')[0].lang;
-            if ($rootScope.language && ($rootScope.language.length > 2)) {
-                $rootScope.language = $rootScope.language.substr(0, 2);
+            if (menu[i].submenu) {
+                registerMenuRoutes(menu[i].submenu);
             }
         }
-        $rootScope.menu = menu;
+    };
+    registerMenuRoutes(menu);
+    $routeProvider.
+        when('/bonsai/', {templateUrl: '/partials/main.html', controller: 'bonsaiCpuCtrl'}).
+        when('/assembler/', {templateUrl: '/partials/assembler.html', controller: 'bonsaiAssemblerCtrl'}).
+        when('/bonsai/assembler/', {templateUrl: '/partials/assembler.html', controller: 'bonsaiAssemblerCtrl'});
+    $routeProvider.otherwise({templateUrl: '/partials/introduction.html', controller: 'bonsaiPageCtrl'});
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true).hashPrefix('!');
+}]);
+
+bonsaiApp.factory('$exceptionHandler', function ($injector) {
+    return function (exception, cause) {
+        if (exception.localization) {
+            $injector.get('$rootScope').$broadcast('error', exception);
+        } else {
+            if (cause) {
+                console.log("Cause: " + cause);
+            }
+            throw exception;
+        }
+    };
+});
+
+bonsaiApp.run(function ($rootScope, $window, $document, $location, localize, PageCounter) {
+    // root scope functions
+    $rootScope.getLanguages = function () {
+        return ['en', 'de'];
+    };
+    $rootScope.setLanguage = function (lang) {
+        $rootScope.language = lang;
+    };
+    $rootScope.$watch('language', function (newLang) {
+        localize.setLanguage(newLang);
+        $rootScope.$broadcast('langChange', newLang);
     });
+    var findPartial = function (path) {
+        if (path == '/') {
+            return 'introduction.html';
+        }
+        if (path == '/imprint/') {
+            return 'imprint.html';
+        }
+        var findPartialInMenu = function (path, menu) {
+            for (var i = 0; i < menu.length; i++) {
+                if (menu[i].link == path) {
+                    return menu[i].partial
+                }
+                if (menu[i].submenu) {
+                    var found = findPartialInMenu(path, menu[i].submenu);
+                    if (found) {
+                        return found;
+                    }
+                }
+            }
+            return false;
+        };
+        return findPartialInMenu(path, menu);
+    };
+    $rootScope.$watch(
+        function () {
+            return $location.path();
+        },
+        function (path) {
+            PageCounter.count(path, findPartial(path)).success(function (data) {
+                if (data.count) {
+                    $rootScope.viewsCount = data.count;
+                }
+                if (data.last_change) {
+                    $rootScope.lastChange = new Date(data.last_change * 1000);
+                }
+            });
+            var pathParts = path.split('/');
+            var currentMenu = [];
+            angular.forEach(menu, function (menuEntry) {
+                if (menuEntry.name == pathParts[1]) {
+                    currentMenu = menu;
+                }
+            });
+            if ((path == '/') || (path == '/imprint/')) {
+                currentMenu = menu;
+            }
+            var navigationPath = [];
+            for (var i = 0; i < pathParts.length; i++) {
+                if (pathParts[i]) {
+                    for (var j = 0; j < currentMenu.length; j++) {
+                        if ((currentMenu[j].name == pathParts[i]) && (currentMenu[j].submenu)) {
+                            navigationPath.push({'title': currentMenu[j].title, 'link': currentMenu[j].link});
+                            currentMenu = currentMenu[j].submenu;
+                        }
+                    }
+                }
+            }
+            $rootScope.navigationPath = navigationPath;
+            $rootScope.currentPath = path;
+            $rootScope.menu = currentMenu;
+            $rootScope.author = undefined;
+        }
+    );
+    // initialization
+    if (!$rootScope.language) {
+        $rootScope.language = $window.navigator.userLanguage ||
+        $window.navigator.language ||
+        $document.getElementsByTagName('html')[0].lang;
+        if ($rootScope.language && ($rootScope.language.length > 2)) {
+            $rootScope.language = $rootScope.language.substr(0, 2);
+        }
+    }
+    $rootScope.menu = menu;
+});
